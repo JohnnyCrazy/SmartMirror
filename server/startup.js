c2af835c37d5e7f314116fc1ffcaba4685e1b199
Johnny @PC <johnny@johnnycrazy.de>
2016-01-17 00:14:34 +0100

    Linux fix? v2

--- server/startup.js
+++ server/startup.js
@@ -1,20 +1,32 @@
 Meteor.startup(() => {
     let exec = Meteor.npmRequire('child_process').exec;
+    let os = Meteor.npmRequire('os');
 
-    let proxy = exec(`cd ../../../../../.proxy/; node main.js`, (error, stdout, stderr) => {
-        console.log(`Proxy-Proc: ${stdout}`);
-        console.log(`Proxy-Proc error: ${stderr}`);
-        if (error !== null) {
-            console.log(`Proxy-Proc error: ${error}`);
-        }
-    });
+    if(os.platform() == "win32")
+    {
+        let proxy = exec(`cd ../../../../../.proxy/ & node main.js`, (error, stdout, stderr) => {
+            console.log(`Proxy-Proc: ${stdout}`);
+            console.log(`Proxy-Proc error: ${stderr}`);
+            if (error !== null) {
+                console.log(`Proxy-Proc error: ${error}`);
+            }
+        });
+    } else {
+        let proxy = exec(`cd ../../../../../.proxy/; node main.js`, (error, stdout, stderr) => {
+            console.log(`Proxy-Proc: ${stdout}`);
+            console.log(`Proxy-Proc error: ${stderr}`);
+            if (error !== null) {
+                console.log(`Proxy-Proc error: ${error}`);
+            }
+        });
 
-    //Start Chromium
-    let chromium = exec(`cd ../../../../../; startChromium`, (error, stdout, stderr) => {
-        console.log(`Chromium-Proc: ${stdout}`);
-        console.log(`Chromium-Proc error: ${stderr}`);
-        if (error !== null) {
-            console.log(`Chromium-Proc error: ${error}`);
-        }
-    });
+        //Start Chromium
+        let chromium = exec(`cd ../../../../../; startChromium.sh`, (error, stdout, stderr) => {
+            console.log(`Chromium-Proc: ${stdout}`);
+            console.log(`Chromium-Proc error: ${stderr}`);
+            if (error !== null) {
+                console.log(`Chromium-Proc error: ${error}`);
+            }
+        });
+    }
 });
